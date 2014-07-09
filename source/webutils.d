748ef3f1cea1bc9f00da19f9079231f958f1f256
SkySom <skylar.som@gmail.com>
2014-07-09 17:49:19 -0400

    Added Params and Headers
    They run, but have not be tested as working as expected.

--- source/webutils.d
+++ source/webutils.d
@@ -25,8 +25,28 @@ JSONValue getJson(string url, Param[] params) {
 
 JSONValue getJson(string url, Header[] headers, Param[] params) {
   auto returned = appender!string();
+  // for now this assumes that the program doesn't create two
+  // different param objects with "" for both name and value.
+  if(params.length > 1 || (params.length == 1 && params[0].name == "" && params[0].value == "")) {
+    auto queryUrl = appender!string();
+    queryUrl.put(url);
+    queryUrl.put("?");
+    foreach(Param para; params) {
+      queryUrl.put(para.name);
+      queryUrl.put("=");
+      queryUrl.put(para.value);
+    }
+  }
+
   auto http = HTTP(url);
   http.method = HTTP.Method.get;
+
+  foreach(Header head; headers) {
+    if(head.name != "" && head.value != "") {
+      http.addRequestHeader(head.name, head.value);
+    }
+  }
+
   http.onReceive = (ubyte[] data) {
     foreach(ubyte u; data) {
       returned.put(to!char(u));
