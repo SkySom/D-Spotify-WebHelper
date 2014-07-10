3ed9150383dc899e740f6a47950756ebfedd6e45
SkySom <skylar.som@gmail.com>
2014-07-09 21:52:01 -0400

    Moved some functions to HelperFunction class.
    Also added getting the path for Windows. No Linux yet. #soon

--- source/spotifywebhelper.d
+++ source/spotifywebhelper.d
@@ -1,12 +1,28 @@
 module spotifywebhelper;
 
+import std.stdio;
+import std.path;
 import std.net.curl;
 import std.json;
+import std.process;
 
 class SpotifyWebHelper {
+
+}
+
+class HelperFunctions {
   JSONValue getJson(string url) {
     auto contentString = get(url);
     auto contentJson = parseJSON(contentString);
     return contentJson;
   }
+
+  string getWindowsSpotifyWebHelperPath() {
+    if(!environment.get("USERPROFILE")) {
+      return null;
+    }
+
+    return buildPath(environment.get("USERPROFILE"),
+      "AppData\\Roaming\\Spotify\\Data\\SpotifyWebHelper.exe");
+  }  
 }
